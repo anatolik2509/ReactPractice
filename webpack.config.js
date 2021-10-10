129c592c39ee2fe985f6fb5334fc310ac6bf89b9
realsanya <sashamorozova01@mail.ru>
2021-10-10 21:06:34 +0300

    add some plgns and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -8,6 +10,14 @@ module.exports = {
     },
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: path.resolve(__dirname, 'dist/'),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ]
+        
 }
