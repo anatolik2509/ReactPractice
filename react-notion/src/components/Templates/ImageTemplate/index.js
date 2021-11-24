3b78e7eacf5fe40b7fdfc4be6285deaa6e940660
realsanya <sashamorozova01@mail.ru>
2021-11-25 00:48:41 +0300

    add image dnd

--- react-notion/src/components/Templates/ImageTemplate/index.js
+++ react-notion/src/components/Templates/ImageTemplate/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ImageTemplate = (props) => {
 
@@ -6,6 +6,23 @@ const ImageTemplate = (props) => {
         editMode,
         hide,
     } = props;
+
+    const [drag, setDrag] = useState(false);
+
+    const dragStartHandler = (ev) => {
+        ev.preventDefault();
+        setDrag(true);
+    }
+
+    const dragLeaveHandler = (ev) => {
+        ev.preventDefault();
+        setDrag(false);
+    }
+
+    const onDropHandler = () => {
+        console.log('TODO SAVE IMAGE')
+        setDrag(false);
+    }
 
     const onSave = () => {
         console.log('TODO Save table');
@@ -16,11 +33,27 @@ const ImageTemplate = (props) => {
         <>
             {editMode ? (
                 <>
-                    <h1>TODO Image Template</h1>
+                    {drag 
+                        ? <div 
+                            className="drop-area"
+                            onDragStart={dragStartHandler}
+                            onDragLeave={dragLeaveHandler}
+                            onDragOver={dragStartHandler}
+                            onDrop={onDropHandler}
+                        > Отпустите файлы, чтобы загрузить их </div> 
+                        : <div
+                            className="empty-drop-area"
+                            onDragStart={dragStartHandler}
+                            onDragLeave={dragLeaveHandler}
+                            onDragOver={dragStartHandler}
+                        > Перетащите файлы, чтобы загрузить их </div>
+                    }
                     <button className="block-button" onClick={onSave}> Сохранить </button>
                 </>
             ) : (
-                <h1>This is Image template</h1>
+                <>
+                    <h1>TODO Image Template</h1>  
+                </>     
             )}
         </>
     );
